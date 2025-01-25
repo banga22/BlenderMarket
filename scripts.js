f3593272ee72bc491550e2fc3311ea72161f0a92
banga22 <danilfomin74@gmail.com>
2025-01-25 13:00:01 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,26 +7,3 @@ document.addEventListener('DOMContentLoaded', function() {
         alert(`Спасибо за ваше сообщение, ${name}! Мы свяжемся с вами по адресу ${email}.`);
     });
 });
-
-let currentIndex = 0;
-const carouselItems = document.querySelectorAll('.carousel-item');
-
-function goToSlide(index) {
-  if (index &lt; 0) {
-    index = carouselItems.length - 1;
-  } else if (index &gt;= carouselItems.length) {
-    index = 0;
-  }
-  currentIndex = index;
-  document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 100}%)`;
-}
- 
-function goToNextSlide() {
-  goToSlide(currentIndex + 1);
-}
- 
-function goToPrevSlide() {
-  goToSlide(currentIndex - 1);
-}
- 
-setInterval(goToNextSlide, 3000);
